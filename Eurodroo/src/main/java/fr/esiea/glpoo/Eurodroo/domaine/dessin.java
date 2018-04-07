c3beaf8baab5455591e774646d80b6c639b45c5d
julesAngue <angue@et.esiea.fr>
2018-04-07 16:18:53 +0200

    Interface dessin first version

---
+++ Eurodroo/src/main/java/fr/esiea/glpoo/Eurodroo/domaine/dessin.java
@@ -0,0 +1,20 @@
+package fr.esiea.glpoo.Eurodroo.domaine;
+
+import java.util.List;
+
+public interface dessin {
+
+	public double getAbscisse;
+	
+	public double getOrdonnée;
+	
+	public double getTaille;
+	
+	public List getForme;
+	
+	public List getCouleur;
+	
+	public double getRotation;
+	
+	
+}
