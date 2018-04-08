38c186b429b2254da8330df103fd5f82e858e37d
julesAngue <angue@et.esiea.fr>
2018-04-08 22:08:55 +0200

    add method in Dessin

--- Eurodroo/src/main/java/fr/esiea/glpoo/Eurodroo/ihm/Dessin.java
+++ Eurodroo/src/main/java/fr/esiea/glpoo/Eurodroo/ihm/Dessin.java
@@ -1,5 +1,54 @@
 package fr.esiea.glpoo.Eurodroo.ihm;
 
-public class Dessin {
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.List;
 
+import javax.swing.JPanel;
+
+import org.apache.log4j.Logger;
+
+public class Dessin extends JPanel {
+	
+	private int boule1;
+	private int boule2;
+	private int boule3;
+	private int boule4;
+	private int boule5;
+	private int boule6;
+	private int etoile1;
+	private int etoile2;
+	
+	public void abscisse() {
+		
+	}
+
+	public void ordonnée() {
+		
+	}
+	
+	public void hauteur() {
+		
+	}
+	
+	public void largeur() {
+		
+	}
+	
+	
+	public void plein() {
+		
+	}
+	
+	public void rotation() {
+		
+	}
+	
+	public void couleur(Graphics g) {
+		
+	}
+	
+	public void forme() {
+		
+	}
 }
