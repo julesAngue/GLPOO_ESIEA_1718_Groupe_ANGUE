b842852e40a1edc0c6814d4036cc5784136a4d68
julesAngue <angue@et.esiea.fr>
2018-04-09 19:32:38 +0200

    add formes classes

---
+++ Eurodroo/src/main/java/fr/esiea/glpoo/Eurodroo/ihm/Triangle.java
@@ -0,0 +1,7 @@
+package fr.esiea.glpoo.Eurodroo.ihm;
+
+import javax.swing.JPanel;
+
+public class Triangle extends JPanel {
+
+}
---
+++ Eurodroo/src/main/java/fr/esiea/glpoo/Eurodroo/ihm/fillRectangle.java
@@ -0,0 +1,7 @@
+package fr.esiea.glpoo.Eurodroo.ihm;
+
+import javax.swing.JPanel;
+
+public class fillRectangle extends JPanel{
+
+}
