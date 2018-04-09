7f269830bf7f4b7f32e2e589eb81d2882d25d198
Raguideau <akiyaraguideau@gmail.com>
2018-04-09 21:49:08 +0200

    Merge

--- Eurodroo/src/main/java/fr/esiea/glpoo/Eurodroo/ihm/Triangle.java
+++ Eurodroo/src/main/java/fr/esiea/glpoo/Eurodroo/ihm/Triangle.java
@@ -7,6 +7,22 @@ import java.awt.*;
 
 public class Triangle extends JPanel {
 	
+	private int boule1;
+	private int boule2;
+	private int boule3;
+	private int boule4;
+	private int boule5;
+	private int etoile2;
+
+	public Triangle(int boule1, int boule2, int boule3, int boule4, int boule5, int etoile2) {
+		this.boule1 = boule1;
+		this.boule2 = boule2;
+		this.boule3 = boule3;
+		this.boule4 = boule4;
+		this.boule5 = boule5;
+		this.etoile2 = etoile2;
+	}
+
 	public void paint(Graphics g)  
 	{  
 	 super.paint(g);  
